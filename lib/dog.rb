26da1a410f2f5cd9e7693f8b6f4da6017e4eb1f4
Cloe Kouadjo <armelfli50@gmail.com>
2019-09-21 20:40:30 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -36,6 +36,8 @@ class Dog
   end
   
   def self.find_by_id(id)
-    DB[:conn].execute("SELECT * FROM dogs WHERE id = ?", id)
+    DB[:conn].execute("SELECT * FROM dogs WHERE id = ?", id).map do |row|
+      self.new_from_db
+    end
   end
 end
